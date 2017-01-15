5de9d3783ae187f9c87f5b49fa8f7e610280931d
lauret_s <lauret_s@epitech.eu>
2017-01-15 04:20:10 +0100

    fix bug

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var mime = require('mime');
 var app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
 
 /**
  * Route for authenticated users which return the algorihm.
