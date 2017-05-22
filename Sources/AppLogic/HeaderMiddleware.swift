bdc87d442e3529f3b66f80890621ce3f00ff514d
Max de Dumast <max@antmine.io>
2017-05-22 16:29:04 +0100

    Fix for Cross Origin

--- Sources/AppLogic/HeaderMiddleware.swift
+++ Sources/AppLogic/HeaderMiddleware.swift
@@ -12,9 +12,9 @@ final class HeaderMiddleware : Middleware {
     func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let response = try next.respond(to: request)
 
-        response.headers["Access-Control-Allow-Origin"] = "*"
-        response.headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
-        
+        response.headers["Access-Control-Allow-Origin"] = request.headers["Origin"] ?? "*";
+        response.headers["Access-Control-Allow-Headers"] = "X-Requested-With, Origin, Content-Type, Accept"
+        response.headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, OPTIONS, DELETE, PATCH"
         return response
     }
 }
